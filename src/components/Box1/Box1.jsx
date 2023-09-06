0c978453788e2fe3dd40a62b77a1dfe888afce3e
Vishi Chaurasiya <vishichaurasiya@gmail.com>
2023-09-07 02:19:22 +0530

    Add Box1

--- src/components/Box1/Box1.jsx
+++ src/components/Box1/Box1.jsx
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 
 const Box1 = () => {
+  const [isAnimating, setisAnimating] = useState(false);
+
   return (
     <div className="box-container">
       <motion.div
         className="box"
         animate={{
-          x: "70vw",
-          opacity: 1,
+          x: isAnimating ? "70vw" : 0,
+          opacity: isAnimating ? 1 : 0.5,
+          rotate: isAnimating ? 360 : 0,
         }}
         initial={{
-          opacity: 0.1,
+          opacity: 0.3,
         }}
+        transition={{
+          type: "spring",
+          stiffness: 60,
+        }}
+        onClick={() => setisAnimating(!isAnimating)}
       ></motion.div>
     </div>
   );
